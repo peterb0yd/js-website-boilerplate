8983f6e59c69062d2a736a249939dbbb01e27f77
Peter Boyd <peterboyd192@gmail.com>
2018-09-29 19:19:27 -0400

    remove uglify and update gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+[200~var gulp = require('gulp');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
@@ -10,22 +10,23 @@ var path = require('path');
 gulp.task('default', ['less', 'js']);
 
 gulp.task('less', function () {
-  return gulp.src('./src/public/styles/**/**.less')
-    .pipe(concat('style.css'))
-    .pipe(less({
-      paths: [ path.join(__dirname, 'style.css') ]
-    }))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./src/public/_css'));
+	  return gulp.src('./src/public/styles/**/**.less')
+	    .pipe(concat('style.css'))
+	    .pipe(less({
+		          paths: [ path.join(__dirname, 'style.css') ]
+		        }))
+	    .pipe(cleanCSS({compatibility: 'ie8'}))
+	    .pipe(gulp.dest('./src/public/_css'));
 });
 
 
 gulp.task('js', function() {
-    return gulp.src('./src/public/js/**.js')
-      .pipe(concat('scripts.js'))
-      .pipe(babel({
-          presets: ['es2015']
-      }))
-      .pipe(uglify())
-      .pipe(gulp.dest('./src/public/_javascript'));
+	    return gulp.src('./src/public/js/**.js')
+	      .pipe(concat('scripts.js'))
+	      .pipe(babel({
+		                presets: ['es2015']
+		            }))
+	      .pipe(uglify())
+	      .pipe(gulp.dest('./src/public/_javascript'));
 });
+
